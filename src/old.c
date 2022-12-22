57cc393bca64d44b69ad6d3af601de4f23a12618
Oleg <slepcov_oleg@mail.ru>
2022-12-22 12:55:55 +0700

    +

--- src/old.c
+++ src/old.c
@@ -4,15 +4,27 @@
 #include <limits.h>
 
 
+#define BITS_IN_INT 31          // кол-во бит в инте
+#define INTS_IN_DECIMAL 3       // кол-во INT хранящих значащее число
+#define META_NUMBER_INT 3       // номер INT хранящий знак и порядок в decimal
+#define BIT_SIGN BITS_IN_INT    // номер бита знака в INT
+#define BIT_EXP_START 16        // номер бита с которого хранится экспонента
+#define BIT_EXP_END 23          // номер юита до которого хранится экспонента
+#define EXP_MAX 28              // максимальное значение экспоненты
+#define EXP_MIN 0               // минимальное значение экспоненты
 
-
+bool sum_decimal(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+bool sum_arr_ints(int* value_1, int* value_2, int* result);
 bool sum_ints(int value_1, int value_2, int* result, bool transfer_bit);
 
+bool left_shift_ints(int* number, int offset);
+void copy_ints(int* src, int* dst);
 int get_sign(s21_decimal value);
 void set_sign(s21_decimal* value, int sign);
+int get_sum_exponent(s21_decimal value_1, s21_decimal value_2);
 int get_exponent(s21_decimal value);
 void set_exponent(s21_decimal* value, int exp);
-int get_sum_exponent(s21_decimal value_1, s21_decimal value_2);
+
 // РАБОТА С БИТАМИ - нумерация с нуля
 bool get_bit_decimal(s21_decimal value, int byte, int ind);
 bool set_bit_decimal(s21_decimal* value, int byte, int ind);
@@ -20,31 +32,49 @@ bool reset_bit_decimal(s21_decimal* value, int byte, int ind);
 bool get_bit_int(int number, int ind);
 bool set_bit_int(int* number, int ind);
 bool reset_bit_int(int* number, int ind);
+
 // ФУНКЦИИ ВЫВОДА 
 void print_decimal(s21_decimal value);
+void print_ints(int* value);
 void print_byte(int number);
 
 
 int main() {
-    s21_decimal a = {{0, 0, 0,  5 << 16}};
-    s21_decimal b = {{0, 0, 0,  9 << 16}};
+    s21_decimal a = {{0, 3, 1,  5 << 16}};
+    s21_decimal b = {{0, 32, 1,  9 << 16}};
     s21_decimal c = {{0, 0, 0,  0}};
-    a.bits[0] = 32423;
-    b.bits[0] = 234313212;
+    a.bits[0] = 255;
+    b.bits[0] = 3223423;
     s21_mul(a, b, &c);
     print_decimal(a);
     print_decimal(b);
     print_decimal(c);
 
-    printf("%u\n", c.bits[0]);
-    printf("%u\n", a.bits[0] + b.bits[0]);
+    // printf("exp_a = %u\n", get_exponent(a));
+    // printf("exp_b = %u\n", get_exponent(b));
+    // printf("exp_c = %u\n", get_exponent(c));
+    // printf("%u\n", get_sum_exponent(a, b));
+    // printf("axp = %u\n", get_exponent(a));
+    // printf("a*b = %u\n", c.bits[0]);
+    // printf("%u\n", INT_MAX);
+    // printf("%u\n", UINT_MAX);
     
+    // // print_decimal(a);
+    // // print_decimal(b);
+    // // sum_decimal(a, b, &c);
+    // int d[3] = {2147483647, 3, 2007483647};
+    // int c[3] = {0, 0, 0};
+
+    // copy_ints(d, c);
+    // printf("transfer = %d\n", mul(c, 1));
+    // print_ints(d);
+    // print_ints(c);
     return 0;
 }
 
 
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-    int status = 0;
+    ArithmeticStatus status = OK;
     int offset_value[3] = {0, 0, 0};
     // Знак числа
     if (get_sign(value_1) != get_sign(value_2)) {
@@ -72,11 +102,11 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     return status;
 }
 
-
 int get_sum_exponent(s21_decimal value_1, s21_decimal value_2) {
     int exp_1 = get_exponent(value_1);
     int exp_2 = get_exponent(value_2);
-    int exp = exp_1 + exp_2;
+    int exp = 0;
+    sum_ints(exp_1, exp_2, &exp, false);
     return exp;
 }
 
@@ -102,12 +132,65 @@ void set_sign(s21_decimal* value, int sign) {
     }
 }
 
+void copy_ints(int* src, int* dst) {
+    for (int i = 0; i < INTS_IN_DECIMAL; i++) {
+        dst[i] = src[i];
+    }
+}
+
+bool left_shift_ints(int* number, int offset) {
+    bool transfer = false;
+    bool prev_bit = false;
+    bool state_bit = false;
+    for (int i = 0; i < offset; i++) {
+        for (int j = 0; j < INTS_IN_DECIMAL; j++) {
+            state_bit = number[j] >> BITS_IN_INT;
+            number[j] <<= 1;
+            number[j] |= prev_bit;
+            prev_bit = state_bit;
+        }
+        if (!transfer && prev_bit) {
+            transfer = true;
+        }
+        prev_bit = false;
+        state_bit = false;
+    }
+    return transfer;
+}
+
+bool right_shift_ints(int* number, int offset) {
+    bool transfer = true;
+    bool prev_bit = false;
+    bool state_bit = false;
+
+    for (int i = 0; i < offset; i++) {
+        for (int j = 0; j < INTS_IN_DECIMAL; j++) {
+            number[j] >> 1;
+            if (j < INTS_IN_DECIMAL - 1) {
+                set_bit_int();
+                get_bit_int(number[j + 1], 0);
+            }
+            // state_bit = number[j] >> BITS_IN_INT;
+            // number[j] <<= 1;
+            // number[j] |= prev_bit;
+            // prev_bit = state_bit;
+        }
+
+    }
+    return transfer;
+}
+
 
 // ФУНКЦИИ СЛОЖЕНИЯ
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
+bool sum_decimal(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
+    bool transfer_bit = sum_arr_ints(value_1.bits, value_2.bits, result->bits);
+    return transfer_bit;
+}
+
+bool sum_arr_ints(int* value_1, int* value_2, int* result) {
     bool transfer_bit = false;
     for (int i = 0; i < INTS_IN_DECIMAL; i++) {
-        transfer_bit = sum_ints(value_1.bits[i], value_2.bits[i], &(result->bits[i]), transfer_bit);
+        transfer_bit = sum_ints(value_1[i], value_2[i], &(result)[i], transfer_bit);
     }
     return transfer_bit;
 }
@@ -134,26 +217,24 @@ bool sum_ints(int value_1, int value_2, int* result, bool transfer_bit) {
 
 // РАБОТА С БИТАМИ - нумерация с нуля
 bool get_bit_decimal(s21_decimal value, int byte, int ind) { 
-    return value.bits[byte] & (1 << ind);
-}
-
-bool set_bit_decimal(s21_decimal* value, int byte, int ind) {
-    value->bits[byte] |= 1 << ind;
-    return true;
-}
-
-bool reset_bit_decimal(s21_decimal* value, int byte, int ind) {
-    value->bits[byte] &= ~(1 << ind);
-    return true;
+    return get_bit_int(value.bits[byte], ind);
 }
 
 bool get_bit_int(int number, int ind) { 
     return number & (1 << ind);
 }
 
+bool set_bit_decimal(s21_decimal* value, int byte, int ind) {  
+    return set_bit_int(&(value->bits[byte]), ind);
+}
+
 bool set_bit_int(int* number, int ind) {  
     *number |= 1 << ind;
     return true;
+}
+
+bool reset_bit_decimal(s21_decimal* value, int byte, int ind) {  
+    return reset_bit_int(&(value->bits[byte]), ind);
 }
 
 bool reset_bit_int(int* number, int ind) {  
@@ -170,9 +251,20 @@ void print_decimal(s21_decimal value) {
     printf("\n");
 }
 
-void print_byte(int number) {
-    for (int i = BITS_IN_INT; i >= 0; i--) {
-        printf("%d", (number & (1 << i)) >> i);
+void print_ints(int* value) {
+    for (int i = INTS_IN_DECIMAL - 1; i >= 0; i--) {
+        print_byte(value[i]);
     }
+    printf("\n");
 }
+
+void print_byte(int number) {
+    unsigned int b;
+    for (int i = 31; i >= 0; i--) {
+        b = number & (1 << i);
+        printf("%d", b >> i);
+    }
+   
+}
+
 
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,11 +1,39 @@
-typedef struct 
-{
-    // int sign: 1;
-    // int empty_1: 7;
-    // int exponent: 8;
-    // int empty_2: 16;
+
+#define BITS_IN_INT 31          // кол-во бит в инте
+#define INTS_IN_DECIMAL 3       // кол-во INT хранящих значащее число
+#define META_NUMBER_INT 3       // номер INT хранящий знак и порядок в decimal
+#define BIT_SIGN BITS_IN_INT    // номер бита знака в INT
+#define BIT_EXP_START 16        // номер бита с которого хранится экспонента
+#define BIT_EXP_END 23          // номер юита до которого хранится экспонента
+#define EXP_MAX 28              // максимальное значение экспоненты
+#define EXP_MIN 0               // минимальное значение экспоненты
+
+typedef struct {
     int bits[4];
 } s21_decimal;
+
+typedef enum ArithmeticStatus {
+    OK,         // 0 - OK
+    INF_POSIT,  // 1 - число слишком велико или равно бесконечности
+    INF_NEGAT,  // 2 - число слишком мало или равно отрицательной бесконечности
+    DIV_ZERO    // 3 - деление на 0
+} ArithmeticStatus;
+
+typedef enum CompareStatus {
+    FALSE,      // 0 - FALSE
+    TRUE        // 1 - TRUE
+} CompareStatus;
+
+// typedef enum ConversionStatus {
+//     OK,         // 0 - OK
+//     ERR         // 1 - ошибка конвертации
+// } ConversionStatus;
+
+// typedef enum CalcStatus {
+//     OK,         // 0 - OK
+//     ERR         // 1 - ошибка вычисления
+// } CalcStatus;
+
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
@@ -29,3 +57,6 @@ int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
 int s21_truncate(s21_decimal value, s21_decimal *result);
 int s21_negate(s21_decimal value, s21_decimal *result);
+
+
+
